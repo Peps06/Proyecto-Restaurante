a9ea590bc9b0c033423bb65c98b260e84ab7f486
dana044 <dana044car@gmail.com>
2026-04-20 07:35:37 -0600

    Ventana Registro  actualizada

--- src/main/java/com/mycompany/restaurante/App.java
+++ src/main/java/com/mycompany/restaurante/App.java
@@ -11,8 +11,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         //Pruebas de uso
-//        URL resource = getClass().getResource("/com/mycompany/restaurante/fxml/PedidoMeseroPantalla.fxml");
-        URL resource = getClass().getResource("/com/mycompany/restaurante/fxml/CobrarPantalla.fxml");
+        URL resource = getClass().getResource("/com/mycompany/restaurante/fxml/VentanaRegistro.fxml");
+//        URL resource = getClass().getResource("/com/mycompany/restaurante/fxml/CobrarPantalla.fxml");
 
         if (resource == null) {
             throw new RuntimeException("No se encuentra el FXML. Revisa que esté en src/main/resources/com/mycompany/restaurante/");
